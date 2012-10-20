fa2b671886ebb608fdfd0fb4218bc0cc5df181ba
aurelien <aurelien.richez@gmail.com>
2012-10-20 11:12:16 +0200

    removed addNewSubSection

--- src/model/low/document/HasSubSection.java
+++ src/model/low/document/HasSubSection.java
@@ -7,34 +7,30 @@ public interface HasSubSection {
 	 * @return the number of subSections.
 	 */
 	public int getSubSectionNb();
-	
-	/**
-	 * Add a new empty subsection after the other subsections.
-	 */
-	public void addNewSubSection();
-	
-	/**
-	 * Add a new empty subsection at the given position (starting at 0)
-	 * @param at the position of the new section
-	 */
-	public void addNewSubSection(int at);
-	
+
 	/**
 	 * Add a subsection after the other subsections.
-	 * @param subSection the subsection to add.
+	 * 
+	 * @param subSection
+	 *            the subsection to add.
 	 */
 	public void addSubSection(Section subSection);
-	
+
 	/**
 	 * Add a subsection at the given position (starting at 0)
-	 * @param subSection the subsection to add.
-	 * @param at the position of the section
+	 * 
+	 * @param subSection
+	 *            the subsection to add.
+	 * @param at
+	 *            the position of the section
 	 */
 	public void addSubSection(Section subSection, int at);
-	
+
 	/**
 	 * Delete the subsection
-	 * @param i the number of the section to delete, starting at 0
+	 * 
+	 * @param i
+	 *            the number of the section to delete, starting at 0
 	 */
 	public void deleteSubSection(int i);
 }
--- src/model/low/documentImp/HasSubSectionImp.java
+++ src/model/low/documentImp/HasSubSectionImp.java
@@ -8,13 +8,14 @@ import model.low.document.TextIntro;
 
 public abstract class HasSubSectionImp implements HasSubSection {
 
-
 	private List<Section> sections;
-	
+
 	@Override
 	public Section getSubSection(int i) {
-		if(i >= this.sections.size() || i < 0) {
-			throw new DocumentException("No such subsection. This document has " + this.sections.size() + "sections");
+		if (i >= this.sections.size() || i < 0) {
+			throw new DocumentException(
+					"No such subsection. This document has "
+							+ this.sections.size() + "sections");
 		}
 		return this.sections.get(i);
 	}
@@ -24,8 +25,6 @@ public abstract class HasSubSectionImp implements HasSubSection {
 		return this.sections.size();
 	}
 
-	
-
 	@Override
 	public void addSubSection(Section subSection) {
 		this.sections.add(subSection);
@@ -34,18 +33,6 @@ public abstract class HasSubSectionImp implements HasSubSection {
 	@Override
 	public void deleteSubSection(int i) {
 		this.sections.remove(i);
-	}
-
-	@Override
-	public void addNewSubSection() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void addNewSubSection(int at) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
