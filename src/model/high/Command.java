5489513b016862ec3866d89c0fdae612dbb76edd
aurelien <aurelien.richez@gmail.com>
2012-10-21 22:12:39 +0200

    added javadoc

--- src/model/high/Command.java
+++ src/model/high/Command.java
@@ -2,8 +2,15 @@ package model.high;
 
 public interface Command {
 	
+	/**
+	 * @param str the textual command.
+	 * @return true if the sequence is interpretable by this command
+	 */
 	public boolean match(CharSequence str);
 
-	public boolean execute(CharSequence str);
+	/**
+	 * @param str the textual command.
+	 */
+	public void execute(CharSequence str);
 	
 }
--- src/model/high/Editor.java
+++ src/model/high/Editor.java
@@ -5,13 +5,41 @@ import model.low.document.Text;
 
 public interface Editor {
 	
+	/**
+	 * @return the section where the cursor is.
+	 */
 	public Section getSelectedSection();
 	
+	/**
+	 * @return the Text where the cursor is.
+	 */
 	public Text getSelectedText();
 	
+	/**
+	 * @return The line number where the cursor is in the selected Text.
+	 */
 	public int getSelectedLineNb();
 	
+	/**
+	 * @return The index of the cursor in the line.
+	 */
 	public int getSelectedCharacterNb();
 	
-	public int addCommand(Command c);
+	/**
+	 * Add a new Command in the editor. The user will then be able to call this command.
+	 * @param c the command to add.
+	 */
+	public void addCommand(Command c);
+	
+	/**
+	 * Execute the textual command.
+	 * @param c the command.
+	 */
+	public void executeCommand(String c);
+	
+	/**
+	 * Load the given document.
+	 * @param filePath path to the document.
+	 */
+	public void loadDocument(CharSequence filePath);
 }
