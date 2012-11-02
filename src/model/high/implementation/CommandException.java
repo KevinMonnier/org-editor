75b13ecf08010ba46dd5f242dacec211b80a8ce2
aurelien <aurelien.richez@gmail.com>
2012-11-02 19:02:55 +0100

    implemented copyline

---
+++ src/model/high/implementation/CommandException.java
@@ -0,0 +1,26 @@
+package model.high.implementation;
+
+public class CommandException extends RuntimeException {
+
+	public CommandException() {
+		super();
+	}
+
+	public CommandException(String arg0, Throwable arg1, boolean arg2,
+			boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+	public CommandException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
+
+	public CommandException(String arg0) {
+		super(arg0);
+	}
+
+	public CommandException(Throwable arg0) {
+		super(arg0);
+	}
+
+}
---
+++ src/model/high/implementation/CopyLine.java
@@ -0,0 +1,35 @@
+package model.high.implementation;
+
+import model.high.command.Command;
+import model.high.editor.Editor;
+import model.low.buffer.Buffer;
+import model.low.document.Line;
+
+public class CopyLine implements Command {
+	
+	private Editor editor;
+	private Buffer buffer;
+	
+
+	public CopyLine(Editor editor, Buffer buffer) {
+		super();
+		this.editor = editor;
+		this.buffer = buffer;
+	}
+
+	@Override
+	public boolean match(CharSequence str) {
+		return str.equals(EditCommands.COPY);
+	}
+
+	@Override
+	public void execute(CharSequence str) {
+		if(!this.match(str))
+			throw new CommandException("this function does not match " + str);
+		
+		Line line = this.editor.getSelectedLine();
+		buffer.push(line);
+		
+	}
+
+}
