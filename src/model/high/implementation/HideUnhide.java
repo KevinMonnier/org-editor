ffcf0097f61f7ad167b3efe134aa206d810c2e78
aurelien <aurelien.richez@gmail.com>
2012-11-03 13:06:19 +0100

    aded HideUnhide

---
+++ src/model/high/implementation/HideUnhide.java
@@ -0,0 +1,32 @@
+package model.high.implementation;
+
+import model.high.command.Command;
+import model.high.editor.Editor;
+import model.low.document.HasSubSection;
+import model.low.document.Section;
+
+public class HideUnhide implements Command {
+
+	Editor editor;
+	
+	
+	
+	public HideUnhide(Editor editor) {
+		super();
+		this.editor = editor;
+	}
+
+	@Override
+	public boolean match(CharSequence str) {
+		return str.equals(EditCommands.HIDE_OR_SHOW);
+	}
+
+	@Override
+	public void execute(CharSequence str) {
+		HasSubSection selectedItem = editor.getSelectedItem();
+		if(selectedItem instanceof Section) {
+			((Section) selectedItem).getState().showHide((Section) selectedItem);
+		}
+	}
+
+}
